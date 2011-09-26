d5ceb8576b537c91c7ee2235d93744796bf85166
ciembor <pub@ciemborowicz.pl>
2011-09-26 19:22:23 +0200

    Dodano opis instalacji, wsparcie dla wielu przeglądarek oraz poprawiono poważny błąd powodujący, że ukrywane były tylko linki sponsorowane (działało, ale musiałem kiedyś zepsuć).

---
+++ README.md
@@ -0,0 +1,14 @@
+#wykopSpamBlock
+
+## Opis
+
+Skrypt __wykopSpamBlock__ ukrywa wszystkie polecane oraz sponsorowane wykopy (linki partnerskie) w serwisie wykop.pl 3.0.
+Wspierane przeglądarki: __Google Chrome__, __Mozilla Firefox__.
+
+## Instalacja w Google Chrome
+
+Aby zainstalować skrypt w przeglądarce Google Chrome, wystarczy wpisać w przeglądarce adres pliku ze skryptem __wykopspamblock.user.js__.
+
+## Instalacja w Mozilla Firefox
+
+Aby zainstalować skrypt w przeglądarce Mozilla Firefox, trzeba najpierw zainstalować dodatek __GreaseMonkey__ (bądź jego odpowiednik). Następnie, po upewnieniu się, że wspomniany dodatek jest włączony, należy wpisać w przeglądarce adres pliku ze skryptem __wykopspamblock.user.js__.
--- wykopspamblock.user.js
+++ wykopspamblock.user.js
@@ -8,26 +8,32 @@
 // @include        http://www.*wykop.pl/hity* 
 // ==/UserScript==
 
-function block(element) {
-  
-  element.style.display = 'none';
-  console.log('Wykop kurwa!');
-  
-}
+(function() {
 
-////////////////////////////////////////////////////////////////////////
-
-var entries = document.getElementById('body-con').getElementsByClassName('entry');
-var filter = new RegExp('^http://www.wykop.pl/link/partnerredirect/');
-
-////////////////////////////////////////////////////////////////////////
-
-for (i = 0; i < entries.length; i++) {
-  
-  var link = entries.item(i).getElementsByTagName('div').item(0).getElementsByClassName('content').item(0).getElementsByTagName('header').item(0).getElementsByTagName('p').item(0).getElementsByTagName('a').item(1).href;
-
-  if (filter.test(link)) {
-    block(entries.item(i));
+  function block(element) {
+    
+    element.style.display = 'none';
+    console.log('Wykop kurwa!');
+    
   }
+
+  //////////////////////////////////////////////////////////////////////
+
+  var Filter = new RegExp('^http://www.wykop.pl/link/partnerredirect/');
+  var Entries = document.getElementById('body-con').getElementsByClassName('entry');
+  var Links;
+
+  //////////////////////////////////////////////////////////////////////
+
+  for (i = 0; i < Entries.length; i++) {
+    
+    Links = Entries.item(i).getElementsByTagName('div').item(0).getElementsByClassName('content').item(0).getElementsByTagName('header').item(0).getElementsByTagName('p').item(0).getElementsByTagName('a');
+
+    if (Filter.test(Links.item(1).href) || Filter.test(Links.item(2).href)) {
+      block(Entries.item(i));
+    }
+    
+  }
+
+})();
   
-}
