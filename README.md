e8ecfcbbc9a705a4bb87d1f0521df4d4f0cbe218
ciembor <pub@ciemborowicz.pl>
2011-10-02 00:44:36 +0200

    Dodano nową formę reklamy (linki partnerskie i linki sponsorowane to jednak nie to samo).

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 ## Opis
 
-Skrypt __wykopSpamBlock__ ukrywa wszystkie polecane oraz sponsorowane wykopy (linki partnerskie) w serwisie wykop.pl 3.0.
+Skrypt __wykopSpamBlock__ ukrywa partnerskie (w tym Wykop Poleca) oraz sponsorowane (S) linki w serwisie wykop.pl 3.0.
 Wspierane przeglądarki: __Google Chrome__, __Mozilla Firefox__.
 
 ## Instalacja w Google Chrome
 
-Aby zainstalować skrypt w przeglądarce Google Chrome, wystarczy wpisać w przeglądarce adres pliku ze skryptem __wykopspamblock.user.js__.
+Aby zainstalować skrypt w przeglądarce Google Chrome, wystarczy wpisać w pasku adresu adres pliku ze skryptem __wykopspamblock.user.js__.
 
 ## Instalacja w Mozilla Firefox
 
-Aby zainstalować skrypt w przeglądarce Mozilla Firefox, trzeba najpierw zainstalować dodatek __GreaseMonkey__ (bądź jego odpowiednik). Następnie, po upewnieniu się, że wspomniany dodatek jest włączony, należy wpisać w przeglądarce adres pliku ze skryptem __wykopspamblock.user.js__.
+Aby zainstalować skrypt w przeglądarce Mozilla Firefox, trzeba najpierw zainstalować dodatek __GreaseMonkey__ (bądź jego odpowiednik). Następnie, po upewnieniu się, że wspomniany dodatek jest włączony, należy wpisać w pasku adresu adres pliku ze skryptem __wykopspamblock.user.js__.
--- wykopspamblock.user.js
+++ wykopspamblock.user.js
@@ -5,35 +5,90 @@
 // @include        http://www.*wykop.pl/
 // @include        http://www.*wykop.pl/strona/*
 // @include        http://www.*wykop.pl/wykopalisko*
-// @include        http://www.*wykop.pl/hity* 
+// @include        http://www.*wykop.pl/hity*
 // ==/UserScript==
 
 (function() {
-
+  
   function block(element) {
     
     element.style.display = 'none';
-    console.log('Wykop kurwa!');
     
   }
 
   //////////////////////////////////////////////////////////////////////
+  
+  var i;
+  var key;
+  var entries = document.querySelectorAll('#body-con .entry');
+  var header;
+  
+  //////////////////////////////////////////////////////////////////////
+  
+  // the type of an advert
+  function AdType(filter) {
+    
+    this.filter = filter;
+    this.href = undefined;
+    
+  }
+    
+  AdType.prototype.setHref = function(link) {
+    
+    if (link instanceof HTMLElement) {
+      this.href = link.href;
+    }
+      
+  };
 
-  var Filter = new RegExp('^http://www.wykop.pl/link/partnerredirect/');
-  var Entries = document.getElementById('body-con').getElementsByClassName('entry');
-  var Links;
+  AdType.prototype.test = function() {
+     
+    if (this.href !== undefined) {
+      return this.filter.test(this.href);
+    }
+    else {
+      return false;
+    }
+
+  };
+      
+  // container with regular expressions
+  var filters = {
+      
+    partner: new RegExp('^http://www.wykop.pl/link/partnerredirect/'),
+    paylink: new RegExp('^http://www.wykop.pl/paylink/')
+      
+  };
+    
+  // container with advert types
+  var adTypes = {
+      
+    partner: new AdType(filters.partner),
+    wykopPoleca: new AdType(filters.partner),
+    sponsorowany: new AdType(filters.paylink)
+      
+  };
 
   //////////////////////////////////////////////////////////////////////
 
-  for (i = 0; i < Entries.length; i++) {
-    
-    Links = Entries.item(i).getElementsByTagName('div').item(0).getElementsByClassName('content').item(0).getElementsByTagName('header').item(0).getElementsByTagName('p').item(0).getElementsByTagName('a');
+  for (i = 0; i < entries.length; ++i) {
 
-    if (Filter.test(Links.item(1).href) || Filter.test(Links.item(2).href)) {
-      block(Entries.item(i));
+    // first div > .content > header
+    header = entries.item(i).getElementsByTagName('div').item(0).querySelectorAll('.content header').item(0);
+
+    adTypes.partner.setHref(header.querySelectorAll('p a').item(1));
+    adTypes.wykopPoleca.setHref(header.querySelectorAll('p a').item(2));
+    adTypes.sponsorowany.setHref(header.querySelectorAll('h2 a').item(0));
+
+    for (key in adTypes) {
+          
+      if (adTypes[key].test()) {
+        console.log("blokuję " + key);
+        block(entries.item(i));
+      }
+      
     }
     
   }
 
 })();
-  
